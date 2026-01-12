55b470fd2b46ed82c96db6106e3bc40aaf8c3cd4
WhiteKnight0003 <whileknight0003@gmail.com>
2026-01-12 15:22:01 +0700

    demo profile UI

--- app/src/main/java/com/example/ecommerceapp/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/ecommerceapp/screen/profile/ProfileScreen.kt
@@ -1,5 +1,85 @@
 package com.example.ecommerceapp.screen.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.ecommerceapp.model.UserProfile
 
-class ProfileScreen {
+@Composable
+fun ProfileScreen(
+    onSignOut : () -> Unit // action signout
+) {
+    val currentUser = UserProfile("11", "chung", "h@gmail.com")
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Profile header
+        Spacer(modifier = Modifier.height(32.dp))
+
+        // Profile Picture
+        Box(
+            modifier = Modifier.size(120.dp).clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primaryContainer),
+            contentAlignment = Alignment.Center
+        ){
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Profile Picture",
+                modifier = Modifier.size(80.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+
+        // User info
+        Text(
+            text = currentUser.name,
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = currentUser.email,
+            style = MaterialTheme.typography.headlineMedium,
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = onSignOut,
+            modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.errorContainer,
+                contentColor = MaterialTheme.colorScheme.onErrorContainer
+            )
+        ) {
+            Text(text = "Sign Out", modifier = Modifier.padding(4.dp))
+        }
+    }
 }
