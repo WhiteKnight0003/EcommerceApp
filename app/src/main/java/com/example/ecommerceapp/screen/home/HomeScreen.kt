4a256803325f95835aafd2f690e93f746c5a12c6
WhiteKnight0003 <whileknight0003@gmail.com>
2026-01-12 12:21:15 +0700

    Search bar UI

--- app/src/main/java/com/example/ecommerceapp/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/ecommerceapp/screen/home/HomeScreen.kt
@@ -1,9 +1,18 @@
 package com.example.ecommerceapp.screen.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -12,7 +21,26 @@ fun HomeScreen(){
         topBar = {MyTopAppBar()},
         bottomBar = {BottomNavigationBar()},
     )
-    {
-        Text(text = "Demo")
+    { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(paddingValues)
+        )
+        {
+            // Search section
+            var searchQuery = remember { mutableStateOf("") }
+            val focusManager = LocalFocusManager.current
+            SearchBar(
+                query = searchQuery.value,
+                onQueryChange = {searchQuery.value = it},
+                onSearch = {},
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            )
+
+            // Search result section
+
+            // category section
+
+            // feature product section
+        }
     }
 }
